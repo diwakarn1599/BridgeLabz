3017a0dec3705b2335ecbfff3c070df0624542d6
Diwakar <diwakarn1599@gmail.com>
2021-06-27 23:51:58 +0530

    Abstraction and Encapsulation Completed

---
+++ OopsConceptss/GetNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopsConceptss
+{
+    public class GetNumbers
+    {
+        private int num1, num2;
+
+        public int getNum1()
+        {
+            return num1;
+        }
+        public void setNum1(int num)
+        {
+            this.num1 = num;
+        }
+        public int getNum2()
+        {
+            return num2;
+        }
+        public void setNum2(int num2)
+        {
+            this.num2 = num2;
+        }
+
+    }
+}
--- OopsConceptss/Program.cs
+++ OopsConceptss/Program.cs
@@ -20,7 +20,12 @@ namespace OopsConceptss
             res = obj.GetSum(num1, num2);
             Console.WriteLine(res);
             Console.WriteLine("Inheritance completed");
-            
+            GetNumbers gn = new GetNumbers();
+            gn.setNum1(num1);//setter for num1
+            gn.setNum2(num2);//setter for num2
+            Console.WriteLine(obj.GetSum(gn.getNum1(),gn.getNum2()));//printing sum using functions by getters
+            Console.WriteLine("abstraction , encapsulation completed");
+
 
         }
     }
