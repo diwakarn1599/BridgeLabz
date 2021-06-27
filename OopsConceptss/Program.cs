89ec248d5c8b064c74ef560cf843372a0d4bf2b4
Diwakar <diwakarn1599@gmail.com>
2021-06-27 23:43:00 +0530

    Inheritance Completed

---
+++ OopsConceptss/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OopsConceptss
+{
+    class Addition
+    {
+        public int GetSum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+    class Program : Addition //inheritance
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, res;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Program obj = new Program();
+            res = obj.GetSum(num1, num2);
+            Console.WriteLine(res);
+            Console.WriteLine("Inheritance completed");
+            
+
+        }
+    }
+}
